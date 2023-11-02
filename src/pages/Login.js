30f848819039fe60401186d640bc1ccdd6806da2
kuikui22 <assoonas.mm@gmail.com>
2023-11-02 09:06:02 +0800

    feat:調整登入畫面樣式

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,37 +1,56 @@
 // TODO: 登入取UserID
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Form } from 'semantic-ui-react';
+import { Button, Form, Header } from 'semantic-ui-react';
 
 function Login(props) {
   const { createUser } = props;
   const [userName, setUserName] = useState('');
+  const [userNameError, setUserNameError] = useState(null);
 
   function onUserNameChange(ev, { value }) {
     setUserName(value);
   }
 
   function onSubmit() {
+    if (!userName) {
+      setUserNameError({
+        content: '請輸入名稱',
+        pointing: 'below',
+      });
+    } else {
+      setUserNameError(null);
+    }
+
     createUser(userName);
   }
+
+  useEffect(() => {
+    if (userNameError && userName) {
+      setUserNameError(null);
+    }
+  }, [userName])
 
   return (
     <div className="login">
       <div className="login-content">
-        <p className="title">猜拳遊戲</p>
-          <Form>
-            <Form.Field>
-              <label>名稱</label>
-              <Form.Input
-                type="text"
-                placeholder="請輸入名稱"
-                name="userName"
-                value={userName}
-                onChange={onUserNameChange}
-              />
-            </Form.Field>
-            <Button type='button' onClick={onSubmit}>登入</Button>
-          </Form>
+        <Header as="h1">猜拳遊戲</Header>
+        <Form>
+          <Form.Field>
+            <label>名稱</label>
+            <Form.Input
+              type="text"
+              placeholder="請輸入名稱"
+              name="userName"
+              value={userName}
+              error={userNameError}
+              onChange={onUserNameChange}
+            />
+          </Form.Field>
+          <Button type="button" color="blue" fluid onClick={onSubmit}>
+            加入遊戲
+          </Button>
+        </Form>
       </div>
     </div>
   );
